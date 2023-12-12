c8f3f4cddbe1586cca5beb8a4b0097fed621b584
Dimitar Beshev <beshev1@gmail.com>
2023-12-12 20:30:21 +0200

    initial upload of services component

---
+++ src/React-Shop.Services/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿namespace Services
+{
+    using Infrastructure.Models;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllAsync(CancellationToken cancellationToken);
+
+        Task<ProductModel> CreateAsync(ProductModel product, CancellationToken cancellationToken);
+
+        Task<int> DeleteAsync(string id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/React-Shop.Services/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿namespace Services
+{
+    using AutoMapper;
+    using Data.Entities;
+    using Data.Repositories;
+    using Infrastructure.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ProductService(
+        IProductRepository productRepository,
+        IMapper mapper) : IProductService
+    {
+        private readonly IProductRepository _productRepository = productRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<ProductModel> CreateAsync(ProductModel product, CancellationToken cancellationToken)
+        {
+            var entity = _mapper.Map<ProductEntity>(product);
+            var result = await _productRepository.InsertAsync(entity, cancellationToken);
+
+            return _mapper.Map<ProductModel>(result);
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.WhereAsync(cancellationToken);
+            var result = products.Select(entity => _mapper.Map<ProductModel>(entity));
+
+            return result;
+        }
+
+        public async Task<int> DeleteAsync(string Id, CancellationToken cancellationToken)
+        {
+            return await _productRepository.DeleteManyAsync(x => x.Id.Equals(Id), cancellationToken);
+        }
+    }
+}
